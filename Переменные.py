6f66e6a4005c5a69e84013e3b9a72e47f79add2d
Peskov-Danil <peskovdanil892@gmail.com>
2024-11-19 18:18:36 +0400

    Add files via upload

---
+++ Переменные.py
@@ -0,0 +1,5 @@
+number_of_homework=12
+number_of_hours=1.5
+course_name ='Python'
+time_for_one_homework=number_of_homework/number_of_hours
+print (f'course_name,', всего задач' , number_of_homework,'затрачено часов',number_of_hours,',среднее выолнение',time_for_one_homework,'часа')
